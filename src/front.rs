cb602ffab30dbfa86ebce9aadfd4f7c565524ec3
Leandro-232052432 <232052432@aluno.unb.br>
2025-02-07 17:06:03 -0300

    Recepção de dados e pagina de telemetria

--- src/front.rs
+++ src/front.rs
@@ -1,15 +1,27 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpServer, Responder, HttpResponse, post};
 use handlebars::Handlebars;
 use serde::Serialize;
 use std::env;
-use std::path::PathBuf;
-use std::io;
+use std::path::{Path,PathBuf};
+use std::io::{self, Read, Write};
+use serde_json::{json, Value};
+use std::fs::{self, OpenOptions};
+use crate::{LogEntry, ler_dados_memoria, salvar_dados_memoria};
+
 
 #[derive(Serialize)]
 struct LoginData {}
 
 #[derive(Serialize)]
 struct TelemetryData {}
+
+// Endpoint para retornar os dados
+async fn get_dados() -> impl Responder {
+    match ler_dados_memoria() {
+        Ok(dados) => HttpResponse::Ok().json(dados),
+        Err(e) => HttpResponse::InternalServerError().body(format!("Erro: {}", e)),
+    }
+}
 
 async fn login_page(hb: web::Data<Handlebars<'_>>) -> impl Responder {
     let data = LoginData {};
@@ -32,6 +44,64 @@ async fn telemetry_page(hb: web::Data<Handlebars<'_>>) -> impl Responder {
         }
     }
 }
+
+async fn post_dados(body: web::Json<Value>) -> impl Responder {
+    let file_path = format!("{}/dados/dadosmemoria.json", std::env::current_dir().unwrap().display());
+
+    // Se o arquivo não existir, cria ele
+    if !Path::new(&file_path).exists() {
+        fs::File::create(&file_path).expect("Erro ao criar o arquivo");
+    }
+
+    let mut file_content = String::new();
+    if let Ok(mut file) = OpenOptions::new().read(true).open(&file_path) {
+        file.read_to_string(&mut file_content).unwrap_or(0);
+        println!("Arquivo lido com sucesso!");
+    }
+
+    let mut data: Vec<Value> = if file_content.trim().is_empty() {
+        Vec::new()
+    } else {
+        serde_json::from_str(&file_content).unwrap_or_else(|_| Vec::new())
+    };
+
+    data.push(body.into_inner());
+
+    if let Ok(mut file) = OpenOptions::new().write(true).truncate(true).open(&file_path) {
+        file.write_all(serde_json::to_string_pretty(&data).unwrap().as_bytes()).unwrap();
+        println!("Arquivo escrito com sucesso!");
+    }
+
+    println!("Dados recebidos e salvos: {:?}", data);
+    HttpResponse::Ok().json("Dados salvos com sucesso!")
+}
+
+async fn limpar_dados() -> impl Responder {
+    let file_path = format!("{}/dados/dadosmemoria.json", std::env::current_dir().unwrap().display());
+
+    // Escreve um array vazio no arquivo
+    if let Ok(mut file) = OpenOptions::new().write(true).truncate(true).open(file_path) {
+        file.write_all(b"[]").unwrap();
+    }
+
+    HttpResponse::Ok().json("Dados apagados com sucesso!")
+}
+
+
+// #[post("/dados")]
+// async fn post_dados(body: web::Json<Value>) -> impl Responder {
+//     println!("Recebido: {:?}", body);
+//     HttpResponse::Ok().json("Dados salvos com sucesso")
+// }
+
+// HttpServer::new(move || {
+//     App::new()
+//         .service(
+//             web::resource("/dados")
+//                 .route(web::get().to(get_dados))
+//                 .route(web::post().to(post_dados)) // Aqui corrigido
+//         )
+// })
 
 pub async fn start_frontend() -> io::Result<()> {
     let mut handlebars = Handlebars::new();
@@ -60,11 +130,20 @@ pub async fn start_frontend() -> io::Result<()> {
         }
     }
 
+    
+
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(handlebars.clone()))
             .service(web::resource("/").route(web::get().to(login_page)))
             .service(web::resource("/telemetry").route(web::get().to(telemetry_page)))
+            // Nova rota para fornecer os dados em JSON:
+            .service(
+                web::resource("/dados")
+                    .route(web::get().to(get_dados))
+                    .route(web::post().to(post_dados)) // Aqui corrigido
+                    .route(web::delete().to(limpar_dados))
+            )
     })
     .bind("127.0.0.1:8080")?
     .run()
--- target/debug/Plotacao-graficos.d
+++ target/debug/Plotacao-graficos.d
@@ -1,1 +1,1 @@
-F:\Codigos\Projeto_final_LP\target\debug\Plotacao-graficos.exe: F:\Codigos\Projeto_final_LP\src\front.rs F:\Codigos\Projeto_final_LP\src\main.rs
+/home/leandro/TrabalhoLP/Projeto_final_LP/target/debug/Plotacao-graficos: /home/leandro/TrabalhoLP/Projeto_final_LP/src/front.rs /home/leandro/TrabalhoLP/Projeto_final_LP/src/main.rs
